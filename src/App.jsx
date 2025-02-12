615f1ee25bd4dc1da732d43563a934a71340d7f4
JT <jtbolstad@gmail.com>
2025-02-12 19:45:11 +0100

    For midlertidig testing: Legg til tilfeldig geoposisjon og klikkevent for å sette geoposisjon.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
 import { useEffect, useState } from 'react'
 import { findNearestStationsWithAvailability } from './utils/findNearestStations'
-import { MapContainer, TileLayer, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Popup, useMapEvents } from 'react-leaflet'
 import { fetchAllStationData } from './services/api'
 import BikeMarker from './components/BikeMarker'
 import UserPositionMarker from './components/UserPositionMarker'
 import 'leaflet/dist/leaflet.css'
+import { use } from 'react'
+
+function MapClickHandler({ setUserPosition }) {
+  useMapEvents({
+    click: (e) => {
+      setUserPosition({
+        lat: e.latlng.lat,
+        lng: e.latlng.lng
+      });
+    
+    },
+  });
+  return null;
+}
 
 function App() {
   const [stations, setStations] = useState([])
   const [stationStatus, setStationStatus] = useState({})
   const [userPosition, setUserPosition] = useState(null)
-   
-  const { nearestBike, nearestDock } = userPosition 
+  const [nearestBike, setNearestBike] = useState(null)
+  const [nearestDock, setNearestDock] = useState(null)
+     
+  useEffect(() => {
+    const { nearestBike, nearestDock } = userPosition 
     ? findNearestStationsWithAvailability(userPosition, stations, stationStatus) 
     : { nearestBike: null, nearestDock: null };
+    
+    setNearestBike(nearestBike);
+    setNearestDock(nearestDock);
+  }, [userPosition, stations, stationStatus]) 
 
-    console.log('nearestBike:', nearestBike);
-    console.log('nearestDock:', nearestDock);
-    console.log('userPosition:', userPosition);
-  
+
   useEffect(() => {
+    // Tilfeldige koordinater for testing
+    setUserPosition({
+      lng: 10.7 + (Math.random() * 0.1),
+      lat: 59.9 + (Math.random() * 0.05)
+    });
+    return;
     // Watch user position
     if ("geolocation" in navigator) {
       const watchId = navigator.geolocation.watchPosition(
         (position) => {
-          console.log('setUserPosition:');
           setUserPosition({
-            // lat: position.coords.latitude,
-            // lng: position.coords.longitude
-            lng: 10.7 + (Math.random() * 0.1),
-            lat: 59.9 + (Math.random() * 0.1)
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
           });
         },
         (error) => {
@@ -67,11 +88,11 @@ function App() {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <MapClickHandler setUserPosition={setUserPosition} />
       {userPosition && <UserPositionMarker position={userPosition} />}
       {stations.map(station => {
         const status = stationStatus[station.station_id]
         if (!status) return null
-
         return (
           <BikeMarker
             key={station.station_id}
--- src/components/BikeMarker.jsx
+++ src/components/BikeMarker.jsx
@@ -2,10 +2,16 @@ import { Marker, Popup } from 'react-leaflet'
 import L from 'leaflet'
 import { useEffect, useMemo } from 'react'
 
+// TODO: Add update time to map (remove from popup - seems to be the same for all)
+// TODO: Add refresh button/toggle
+
 function BikeMarker({ station, status, isNearestBike, isNearestDock }) {
   const { lat, lon, name, address } = station
-  const { num_bikes_available, num_docks_available } = status
+
+  const { num_bikes_available, num_docks_available, last_reported, vehicle_types_available} = status;
   const isNearest = isNearestBike || isNearestDock
+  const EBIKETYPE = 1;
+  const esykler = vehicle_types_available[EBIKETYPE].count;
 
   const divIcon = useMemo(() => {
     const hasAvailableBikes = num_bikes_available > 0
@@ -16,8 +22,9 @@ function BikeMarker({ station, status, isNearestBike, isNearestDock }) {
         ${num_bikes_available}/${num_docks_available}
       </div>`
     })
-  }, [num_bikes_available, num_docks_available])
+  }, [num_bikes_available, num_docks_available, isNearest])
 
+  const oppdatert = new Date(last_reported * 1000).toLocaleString();
   return (
     <Marker 
       position={[lat, lon]} 
@@ -28,10 +35,11 @@ function BikeMarker({ station, status, isNearestBike, isNearestDock }) {
         <div>
           <h3>{name}</h3>
           <p>{address}</p>
-          <p>Available bikes: {num_bikes_available}</p>
-          <p>Available docks: {num_docks_available}</p>
+          <p>Ledige sykler: {num_bikes_available}</p>
+          <p>Herav esykler: {esykler}</p>
           <p>Lat: {lat}</p>
-          <p>Lon:: {lon}</p>
+          <p>Lon: {lon}</p>
+          <p>Sist oppdatert: {oppdatert}</p> 
         </div>
       </Popup>
     </Marker>
